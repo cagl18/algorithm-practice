67cdb7eae24075188bd103276f5df2c593c20029
Carlos Gonzalez <cagl18@gmail.com>
2019-09-09 23:21:39 -0400

    completed maxchar and reserveint problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,48 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//===============================================================
+// My Soution
+//===============================================================
+
+function maxChar(str) {
+  const chars = {};
+  let max = 0;
+  let maxChar = '';
+
+  str.split('').map(ch => {
+    chars[ch] = chars[ch] + 1 || 1;
+    if (max < chars[ch]) {
+      max = chars[ch];
+      maxChar = ch;
+    }
+  }); //creating the maxChar object
+
+  return maxChar;
+}
+
+// function maxChar(str) {
+//   //lopp over string and find max occurance of each leter
+//   //which maxChar
+
+//   let max = 0; //max ocurrance
+//   let charMax = ''; // which character
+
+//   const chars = {};
+
+//   for (let ch of str) {
+//     if (!chars[ch]) {
+//       chars[ch] = 1;
+//     } else {
+//       chars[ch]++;
+//     }
+//     if (max < chars[ch]) {
+//       max = chars[ch];
+//       charMax = ch;
+//     }
+//   }
+
+//   return charMax;
+// }
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,44 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//===============================================================
+// My Soution
+//===============================================================
+
+// function reverseInt(n) {
+//   //spit n into sign and abs(n)
+//   //if abs less than 10 , return n
+//   // (reverse number) convert number to String, convert string into Array, reverse Array, convert to string, convert to number
+//   // return sign * reverseAbs
+
+//   const abs = Math.abs(n);
+//   const sign = n / abs;
+
+//   if (abs < 10) {
+//     return n; //edge case
+//   }
+
+//   //reverse number
+//   const revString = String(abs)
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return sign * Number(revString);
+// }
+
+//===============================================================
+// Instructor Soution
+//===============================================================
+
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
