22142f726841c2b6dce7f135b22d1073f82f3367
Carlos Gonzalez <cagl18@gmail.com>
2019-10-06 12:47:12 -0400

    completed levelWith problem

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,54 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+//instructor solution
+function levelWidth(root) {
+  const arr = [root, 's'];
+  const counters = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
+
+// my soluation
+
+// function levelWidth(root) {
+//   if (!root) {
+//     return [0];
+//   }
+
+//   const counters = [0];
+//   const widthArray = [root, 's'];
+//   let temp = null;
+//   let index = 0;
+//   while (widthArray.length > 1) {
+//     temp = widthArray.shift();
+
+//     if (temp === 's') {
+//       index++;
+//       counters[index] = 0;
+//       widthArray.push(temp);
+//     } else {
+//       counters[index]++;
+
+//       if (temp.children) {
+//         widthArray.push(...temp.children);
+//       }
+//     }
+//   }
+
+//   return counters;
+// }
 
 module.exports = levelWidth;
